c02dfe68c8dd9b44c85680b694b8bec56a42adff
DanySA22 <winache22@gmail.com>
2024-03-05 07:25:16 -0800

    Uncomment lines of  code related to apikey env

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const cors = require('cors')
 
 //console.log(process.env.PORT) object that has our .env variables
 const {PORT, API_KEY} = process.env
-// const checkAPIKey = (req, res, next) => {
-//     const apiKey = req.header('API-Key')
-//     if(!apiKey) {
-//         return res.status(401).json({error: 'API key is missing'})
-//     } else if (apiKey !== API_KEY ) {
-//         return res.status(403).json({error: 'API key is invalid'})
-//     }
-//     next()
-// }
-// app.use(checkAPIKey)
+const checkAPIKey = (req, res, next) => {
+    const apiKey = req.header('API-Key')
+    if(!apiKey) {
+        return res.status(401).json({error: 'API key is missing'})
+    } else if (apiKey !== API_KEY ) {
+        return res.status(403).json({error: 'API key is invalid'})
+    }
+    next()
+}
+app.use(checkAPIKey)
 
 app.use(express.json())
 app.use(cors())
