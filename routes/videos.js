eaedbfbcfda411bd43cb5058b56bb1de7ae9c035
DanySA22 <winache22@gmail.com>
2024-03-05 07:22:06 -0800

    Add the rest of the endpoints logic. Add the logic for request the right api key from client side

--- routes/videos.js
+++ routes/videos.js
@@ -2,34 +2,60 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
+//Reading the data from the JSON file
 const data = fs.readFileSync('./data/videos.json', 'utf8')
+
+//Transforming the JSON data into an array of objects
 const dataAsObject = Array.from(JSON.parse(data))
-console.log(dataAsObject[2].title)
-console.log(dataAsObject.find((item) => item.id == "84e96018-4022-434e-80bf-000ce4cd12b8"))
+// console.log(dataAsObject[2].comments)
+// console.log(dataAsObject.find((item) => item.id == "84e96018-4022-434e-80bf-000ce4cd12b8"))
+
+//Route that gets all the videos of the list
 router.get('/', (req, res) =>{
     res.json(dataAsObject)
-    //  res.json(data)
 })
 
+//Route that gets all the details of one video of the list based in the id
 router.get('/:id', (req, res) =>{
-    const oneVideo = dataAsObject.find((item) => {item.id == req.params.id})
+    const oneVideo = dataAsObject.find((item) => item.id == req.params.id)
     res.json(oneVideo)
 })
 
+//Route that post the upload video information
 router.post('/', (req, res) =>{
-    res.send('<h1>hello world 3</h1>')
+    dataAsObject.push(req.body)
+    const dataAsString = JSON.stringify(dataAsObject, null, 2)
+    fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
+    
 })
 
+//Route that post a comment for one of the video of the list
 router.post('/:id/comments', (req, res) =>{
-    res.send('<h1>hello world 3</h1>')
+    const oneVideo = dataAsObject.find((item) => item.id == req.params.id)
+    oneVideo.comments.push(req.body)
+    const dataAsString = JSON.stringify(dataAsObject, null, 2)
+    fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
+    
 })
 
-router.delete('/:videoID/comments/:commentId', (req, res) =>{
-    res.send('<h1>hello world 3</h1>')
+// Route that delete a comment for one of the video of the list based on the id of the comment
+router.delete('/:videoId/comments/:commentId', (req, res) =>{
+    const oneVideo = dataAsObject.find((item) => item.id == req.params.videoId)
+    const indexComment = oneVideo.comments.findIndex((item) => item.id == req.params.commentId)
+    oneVideo.comments.splice(indexComment)
+    const dataAsString = JSON.stringify(dataAsObject, null, 2)
+    fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
 })
 
+// Route that update like on comment for one of the video of the list based on the id of the comment
 router.put('/:videoId/likes', (req, res) =>{
-    res.send('<h1>hello world 3</h1>')
+    const oneVideo = dataAsObject.find((item) => item.id == req.params.videoId)
+    oneVideo.likes = req.body
+    const dataAsString = JSON.stringify(dataAsObject, null, 2)
+    fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
+    
 })
-
+// When someone submit the upload form it post all the object info (including the image source folder
+//on the express app and the name of the image). This also means that we should create a parallel process that when 
+//an image is uploaded get send to this folder on express.app
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const video = require("./routes/Videos.js")
-const {PORT, API_KEY} = process.env
 const fs = require('fs')
+const cors = require('cors')
+
 //console.log(process.env.PORT) object that has our .env variables
-// app.get('/', (req, res) =>{
-//     res.send('<h1>hello world</h1>')
-// })
+const {PORT, API_KEY} = process.env
+// const checkAPIKey = (req, res, next) => {
+//     const apiKey = req.header('API-Key')
+//     if(!apiKey) {
+//         return res.status(401).json({error: 'API key is missing'})
+//     } else if (apiKey !== API_KEY ) {
+//         return res.status(403).json({error: 'API key is invalid'})
+//     }
+//     next()
+// }
+// app.use(checkAPIKey)
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/videos', video)
 app.use(express.static('./public/images'))
 app.listen(PORT)
