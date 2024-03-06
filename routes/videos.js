99156890ad43dc65081d573e6f744c55d9c71f10
DanySA22 <winache22@gmail.com>
2024-03-06 05:02:05 -0800

    Modify the json file with the express static folder source images and video

--- routes/videos.js
+++ routes/videos.js
@@ -7,7 +7,7 @@ const data = fs.readFileSync('./data/videos.json', 'utf8')
 
 //Transforming the JSON data into an array of objects
 const dataAsObject = Array.from(JSON.parse(data))
-// console.log(dataAsObject[2].comments)
+console.log(dataAsObject[2].comments)
 // console.log(dataAsObject.find((item) => item.id == "84e96018-4022-434e-80bf-000ce4cd12b8"))
 
 //Route that gets all the videos of the list
@@ -26,7 +26,7 @@ router.post('/', (req, res) =>{
     dataAsObject.push(req.body)
     const dataAsString = JSON.stringify(dataAsObject, null, 2)
     fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
-    
+    res.json(console.log(req.body))
 })
 
 //Route that post a comment for one of the video of the list
@@ -35,7 +35,7 @@ router.post('/:id/comments', (req, res) =>{
     oneVideo.comments.push(req.body)
     const dataAsString = JSON.stringify(dataAsObject, null, 2)
     fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
-    
+    res.json(console.log('post comment'))
 })
 
 // Route that delete a comment for one of the video of the list based on the id of the comment
@@ -45,15 +45,16 @@ router.delete('/:videoId/comments/:commentId', (req, res) =>{
     oneVideo.comments.splice(indexComment)
     const dataAsString = JSON.stringify(dataAsObject, null, 2)
     fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
+    res.json(console.log('delete comment'))
 })
 
 // Route that update like on comment for one of the video of the list based on the id of the comment
 router.put('/:videoId/likes', (req, res) =>{
     const oneVideo = dataAsObject.find((item) => item.id == req.params.videoId)
-    oneVideo.likes = req.body
+    oneVideo.likes = req.body.likes
     const dataAsString = JSON.stringify(dataAsObject, null, 2)
     fs.writeFileSync('./data/videos.json', dataAsString, 'utf8')
-    
+    res.json(console.log('like video'))
 })
 // When someone submit the upload form it post all the object info (including the image source folder
 //on the express app and the name of the image). This also means that we should create a parallel process that when 
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const video = require("./routes/Videos.js")
+const video = require("./routes/videos.js")
 const fs = require('fs')
 const cors = require('cors')
 
 //console.log(process.env.PORT) object that has our .env variables
 const {PORT, API_KEY} = process.env
-const checkAPIKey = (req, res, next) => {
-    const apiKey = req.header('API-Key')
-    if(!apiKey) {
-        return res.status(401).json({error: 'API key is missing'})
-    } else if (apiKey !== API_KEY ) {
-        return res.status(403).json({error: 'API key is invalid'})
-    }
-    next()
-}
-app.use(checkAPIKey)
+// const checkAPIKey = (req, res, next) => {
+//     const apiKey = req.params.api_key
+//     if(!apiKey) {
+//         return res.status(401).json({error: 'API key is missing'})
+//     } else if (apiKey !== API_KEY ) {
+//         return res.status(403).json({error: 'API key is invalid'})
+//     }
+//     next()
+// }
+// app.use(checkAPIKey)
 
 app.use(express.json())
 app.use(cors())
 app.use('/videos', video)
-app.use(express.static('./public/images'))
+app.use(express.static('./public/images-video'))
 app.listen(PORT)
